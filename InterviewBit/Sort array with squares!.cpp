aab3d8e0c361061b1bcace0e76375c360bb26268
hrmtsasan <61978130+hrmtsasan@users.noreply.github.com>
2021-03-12 17:16:39 +0530

    Delete Sort array with squares!.cpp

--- InterviewBit/Sort array with squares!.cpp
+++
@@ -1,9 +0,0 @@
-vector<int> Solution::solve(vector<int> &A) {
-    vector<int> sq;
-    for(int i=0;i<A.size();i++)
-    {
-        sq.push_back(A[i]*A[i]);
-    }
-    sort(sq.begin(),sq.end());
-    return sq;
-}
